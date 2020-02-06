96da6d8532ed33944192d007c442ddc2c4e59ca0
Devin Goble <devin.goble@outlook.com>
2020-02-06 23:43:30 +0000

    add vuetify

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -9,7 +9,7 @@ const routes = [
     path: "/",
     name: "pets",
     component: Pets,
-    props: true,
+    props: true
   },
   {
     path: "/about",
@@ -18,14 +18,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
