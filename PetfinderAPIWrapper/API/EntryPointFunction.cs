526ff0c6e34eb3ddc36b3d8d48015a57a9016e9a
Devin Goble <devin.goble@outlook.com>
2020-02-06 23:09:32 +0000

    Fix query passthrough bug

--- PetfinderAPIWrapper/API/EntryPointFunction.cs
+++ PetfinderAPIWrapper/API/EntryPointFunction.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
@@ -33,15 +34,18 @@ namespace PetfinderAPIWrapper.API
 
         [FunctionName("pets")]
         public async Task<IActionResult> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "pets/{passthroughRoute}")]HttpRequestMessage req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{passthroughRoute}")]HttpRequestMessage req,
             [DurableClient] IDurableEntityClient client,
             string passthroughRoute)
         {
             var accessToken = await _petfinderAuth.GetAuthTokenAsync(client);
-            var uri = new Uri(Path.Join(_options.PetfinderAPIBaseUrl, passthroughRoute));
+            var query = req.RequestUri.Query;
+            var builder = new UriBuilder(Path.Join(_options.PetfinderAPIBaseUrl, passthroughRoute));
+            builder.Query = query;
+            var uri = builder.Uri;
 
             _logger.LogInformation($"Making Petfinder call: URI: {uri.ToString()}, HttpRequestMessage: {req.RequestUri.ToString()}, Client: {client.TaskHubName}");
-
+            
             _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken.AccessToken}");
             var pfDataMessage = await _httpClient.GetAsync(uri);
 
